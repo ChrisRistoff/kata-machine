9c14b362565dc3a9a73cf8c22f4c508896358bf4
krasen.hristov <krasen.hristov@wrenkitchens.com>
2024-11-21 17:10:36 +0000

    bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = arr.length; j > i; j--) {
+            if (arr[i] > arr[j]) {
+                const temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
