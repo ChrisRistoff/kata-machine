18bb6d075b7765ba43f8b745fdd0f3e42c0eeecf
krasen.hristov <krasen.hristov@wrenkitchens.com>
2024-11-21 16:48:19 +0000

    Binary no recursion

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,15 +1,19 @@
-export default function bs_list(haystack: number[], needle: number, low: number = 0, high: number = haystack.length): boolean {
-    const mid = Math.floor((high + low) / 2);
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
-    if (low >= high) {
-        return false;
-    } else if (haystack[mid] === needle) {
-        return true;
-    } else if (haystack[mid] > needle) {
-        high = mid;
-        return bs_list(haystack, needle, low, high);
-    } else {
-        low = mid + 1;
-        return bs_list(haystack, needle, low, high);
+    while (low < high) {
+        const mid = Math.floor((high + low) / 2);
+        if (haystack[mid] === needle) {
+            return true;
+        }
+
+        if (haystack[mid] > needle) {
+            high = mid;
+        } else if (haystack[mid] < needle) {
+            low = mid + 1;
+        }
     }
+
+    return false;
 }
