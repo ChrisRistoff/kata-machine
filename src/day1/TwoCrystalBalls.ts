1006adac76c8b8e5b32ec02a77e335065cd83f16
krasen.hristov <krasen.hristov@wrenkitchens.com>
2024-11-21 17:07:31 +0000

    crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let step = Math.floor(Math.sqrt(breaks.length));
 
+    for (let i = step; i < breaks.length; i += step) {
+        if (breaks[i]) {
+            return findFirstBreak(breaks, i)!;
+        }
+    }
+
+    return -1;
+}
+
+function findFirstBreak(breaks: boolean[], start: number): number {
+    for (let i = start; i > 0; i--) {
+        if (!breaks[i]) {
+            return i+1;
+        }
+    }
+
+    return 0;
 }
