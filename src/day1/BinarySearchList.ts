6a8093b5605ddb37a5f0968004962a33fd7c4926
ChrisRistoff <doanusho@gmail.com>
2025-04-29 14:20:23 +0100

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,15 +1,21 @@
 export default function bs_list(haystack: number[], needle: number, low: number = 0, high: number = haystack.length): boolean {
-    const mid = Math.floor((high + low) / 2);
+    let mid = Math.floor(low + (high - low) / 2);
+
+    if (haystack[mid] === needle) {
+        return true;
+    }
 
     if (low >= high) {
         return false;
-    } else if (haystack[mid] === needle) {
-        return true;
-    } else if (haystack[mid] > needle) {
-        high = mid;
-        return bs_list(haystack, needle, low, high);
-    } else {
-        low = mid + 1;
-        return bs_list(haystack, needle, low, high);
     }
+
+    if (haystack[mid] > needle) {
+        return bs_list(haystack, needle, low, mid);
+    }
+
+    if (haystack[mid] < needle) {
+        return bs_list(haystack, needle, mid + 1, high);
+    }
+
+    return false;
 }
