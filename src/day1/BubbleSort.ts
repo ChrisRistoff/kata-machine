62659b60d5b597857c8942be18a9fbb8b3d606a8
ChrisRistoff <doanusho@gmail.com>
2025-04-29 14:36:05 +0100

    bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
-export default function bubble_sort(arr: number[]): void {
-
+export default function bubble_sort(array: number[]): void {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = array.length - 1; j > i ; j--) {
+            if (array[i] > array[j]) {
+                const temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
 }
