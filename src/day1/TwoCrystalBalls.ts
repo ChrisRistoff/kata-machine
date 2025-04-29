d8557cf44e16a07db5d5e0656c2ed77d40e0e9eb
ChrisRistoff <doanusho@gmail.com>
2025-04-29 14:31:48 +0100

    two crystal balls
    O(sqrt N)

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let broken = -1;
 
+    for (let i = jumpAmount; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            broken = i;
+            break;
+        }
+    }
+
+    for (let i = broken; i >= 0; i--) {
+        if (!breaks[i]) {
+            return i+1;
+        }
+    }
+
+    return broken;
 }
